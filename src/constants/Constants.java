b930cfc60982bccabe30d80ad5f5617fb24e0b1e
高木侑和 <@AC2025-80-003>
2025-07-23 16:47:33 +0900

    カテゴリー

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -12,7 +12,12 @@ public class Constants {
 	public static final List<String> CATEGORY_LIST = Arrays.asList(
 			"お好み焼き","もんじゃ焼き","鉄板焼き","サイドメニュー","ソフトドリンク","お酒","ボトル");
 	
-	//注文リストで使用するカテゴリー名
-	public static final List<String> ORDER_CATEGORY_LIST = Arrays.asList(
-			"全て","お好み焼き","もんじゃ焼き","鉄板焼き","サイドメニュー","ソフトドリンク","お酒","ボトル");
+	//注文リストのカテゴリー名
+		public static final List<String> ORDER_CATEGORY_LIST = Arrays.asList(
+				"全て","お好み焼き","もんじゃ焼き","鉄板焼き","サイドメニュー","ソフトドリンク","お酒","ボトル","1卓","2卓","3卓");
+		
+	//提供済み履歴のカテゴリー名
+		public static final List<String> HISTORY_TABLE_LIST = Arrays.asList(
+				"全て","1卓","2卓","3卓");
+
 }
