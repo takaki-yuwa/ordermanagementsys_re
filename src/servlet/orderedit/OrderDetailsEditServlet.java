5421c1e0d931f153ceb0557fcc47eb582ba79daa
高木侑和 <@AC2025-80-003>
2025-07-28 13:55:11 +0900

    注文編集からの遷移(仮)

---
+++ src/servlet/orderedit/OrderDetailsEditServlet.java
@@ -0,0 +1,38 @@
+package servlet.orderedit;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/OrderDetailsEditServlet")
+public class OrderDetailsEditServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+			response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+			response.setHeader("Pragma", "no-cache");
+			response.setDateHeader("Expires", 0);
+			
+			String screen = request.getParameter("screen");
+			String orderId = request.getParameter("order_id");
+			
+			System.out.println("screen: " + screen);
+			System.out.println("order_id: " + orderId);
+			
+
+			response.sendRedirect(request.getContextPath() + "/" + URLEncoder.encode(screen, "UTF-8") + "&lang=ja");
+		} catch (Exception e) {
+            e.printStackTrace();
+            request.getRequestDispatcher("/jsp/Error.jsp").forward(request, response);
+        }
+    }
+}
